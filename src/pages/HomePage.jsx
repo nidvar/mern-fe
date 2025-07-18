9dda6365e19d71aef320edf27f8cdfbb9ccb421f
Nidvar <djayshine87@hotmail.com>
2025-07-18 12:29:46 +1000

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -15,7 +15,6 @@ const HomePage = ()=>{
     const changeData = dataState[1];
 
     const grabData = async function(){
-        console.log(baseUrl);
         try{
             setLoading(true);
             const response = await fetch(baseUrl, {
